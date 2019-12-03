ee9a4f96639e7641335bdc26ec1001b85a1b4574
Samuel Pires Brito <47739865+samuelpiresbritoipl@users.noreply.github.com>
2019-12-03 12:52:33 +0000

    Fixed Codeception tests with PHPStorm

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,6 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+
+//This code line is for the codeception tests with PHPStorm
+ob_start();
