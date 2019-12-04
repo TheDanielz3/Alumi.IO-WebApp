fd5154db77f482b47284c6bc49ebfbf756962e65
Samuel Pires Brito <47739865+samuelpiresbritoipl@users.noreply.github.com>
2019-12-04 19:00:58 +0000

    Updated Test Files

---
+++ frontend/tests/unit/TurmaTest.php
@@ -0,0 +1,44 @@
+<?php namespace frontend\tests;
+
+use frontend\models\Turma;
+
+class TurmaTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \frontend\tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testAssertingTrue()
+    {
+        $turma = new Turma();
+        $turma->ano = 1;
+        $turma->letra = "P";
+
+        $this->assertTrue($turma->validate(['ano']));
+        $this->assertTrue($turma->validate(['letra']));
+
+    }
+    public function testAssertingFalse()
+    {
+        $turma = new Turma();
+        $turma->ano = null;
+        $turma->letra = null;
+
+        $this->assertFalse($turma->validate(['ano']));
+        $this->assertFalse($turma->validate(['letra']));
+    }
+    public function testAssertingOnDatabase()
+    {
+
+    }
+}
