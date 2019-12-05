baa11698d30ac2829bb05d8ab73a4a034475dc9d
Daniel Batista <2171836@my.ipleiria.pt>
2019-12-05 17:57:12 +0000

    Testes novos

--- frontend/tests/unit/RecadoTest.php
+++ frontend/tests/unit/RecadoTest.php
@@ -1,12 +1,15 @@
 <?php namespace frontend\tests;
 
-class RecadoTest extends \Codeception\Test\Unit
+use Codeception\Test\Unit;
+use frontend\models\Recado;
+
+class RecadoTest extends Unit
 {
     /**
-     * @var \frontend\tests\UnitTester
+     * @var UnitTester
      */
     protected $tester;
-    
+
     protected function _before()
     {
     }
@@ -16,8 +19,30 @@ class RecadoTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testSomeFeature()
+    public function testAssertingTrue()
     {
+        $recado = new Recado();
+        $recado->descricao = "isto é um recado";
+        // $recado->data =
+    }
+
+    public function testAssertingFalse()
+    {
+        $recado = new Recado();
+        $recado->descricao = null;
+        $recado->data = null;
+        $recado->assinado = null;
+        $recado->id_turma = "error";
+        $recado->id_aluno = "error";
+
+
+        $this->tester->comment("Testing Asserts False");
+
+        $this->assertFalse($recado->validate(['descricao']));
+        $this->assertFalse($recado->validate(['data']));
+        $this->assertFalse($recado->validate(['assinado']));
+        $this->assertFalse($recado->validate(['id_turma']));
+        $this->assertFalse($recado->validate(['id_aluno']));
 
     }
 }
--- frontend/tests/unit/TPCTest.php
+++ frontend/tests/unit/TPCTest.php
@@ -1,12 +1,15 @@
 <?php namespace frontend\tests;
 
-class TPCTest extends \Codeception\Test\Unit
+use Codeception\Test\Unit;
+use frontend\models\Tpc;
+
+class TPCTest extends Unit
 {
     /**
-     * @var \frontend\tests\UnitTester
+     * @var UnitTester
      */
     protected $tester;
-    
+
     protected function _before()
     {
     }
@@ -16,8 +19,25 @@ class TPCTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testSomeFeature()
+    public function testAssertingTrue()
     {
+        $tpc = new Tpc();
+        $tpc->descricao = "Eu sou uma descricao";
+        $tpc->id_disciplina_turma = 1;
 
+        $this->assertTrue($tpc->validate(['descricao']));
+        $this->assertTrue($tpc->validate(['id_disciplina_turma']));
+
+    }
+
+    //Todo: Ver migracao porque ta null no id
+    public function testAssertingFalse()
+    {
+        $tpc = new Tpc();
+        $tpc->descricao = null;
+        $tpc->id_disciplina_turma = "null";
+
+        $this->assertFalse($tpc->validate(['descricao']));
+        $this->assertFalse($tpc->validate(['id_disciplina_turma']));
     }
 }
