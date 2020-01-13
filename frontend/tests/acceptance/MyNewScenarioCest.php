7beb50e334e5d498ffc63e1016a270554447299f
Samuel Pires Brito <47739865+samuelpiresbritoipl@users.noreply.github.com>
2020-01-13 20:24:58 +0000

    Fixed acceptance test.

--- frontend/tests/acceptance/MyNewScenarioCest.php
+++ frontend/tests/acceptance/MyNewScenarioCest.php
@@ -1,5 +1,7 @@
 <?php namespace frontend\tests\acceptance;
 
+use common\fixtures\AuthAssignmentFixture;
+use common\fixtures\ProfessorFixture;
 use common\fixtures\UserFixture;
 use frontend\tests\AcceptanceTester;
 use yii\helpers\Url;
@@ -7,11 +9,31 @@ use yii\helpers\Url;
 class MyNewScenarioCest
 {
     protected $signupFormId = '#form-signup';
+    protected $loginFormId = '#login-form';
+
+    public function _fixtures()
+    {
+        return [
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php',
+            ],
+            'professor' => [
+                'class' => ProfessorFixture::className(),
+                'dataFile' => codecept_data_dir() . 'professor.php'
+            ],
+            'auth_assignment' => [
+                'class' => AuthAssignmentFixture::className(),
+                'dataFile' => codecept_data_dir() . 'auth_assignment.php'
+            ],
+        ];
+    }
 
     public function _before(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/index'));
     }
+
 
     public function tryToSignup(AcceptanceTester $I)
     {
@@ -24,7 +46,7 @@ class MyNewScenarioCest
                 'SignupForm[nome]' => 'acceptanceTestingName',
                 'SignupForm[email]' => 'account@account.account',
                 'SignupForm[password]' => '123456',
-                'SignupForm[user_type]' => 'student',
+                'SignupForm[user_type]' => 'teacher',
             ]
         );
 
@@ -35,6 +57,22 @@ class MyNewScenarioCest
             'status' => \common\models\User::STATUS_INACTIVE
         ]);
 
+    }
+
+
+    public function tryToLogin(AcceptanceTester $I)
+    {
+        $I->click("Login");
+        $I->wait(1);
+
+        $I->submitForm(
+            $this->loginFormId, [
+                'LoginForm[username]'  => 'acceptanceTestingUsername',
+                'LoginForm[password]'  => '123456',
+            ]
+        );
+
+        $I->amOnPage('site/dashboard');
     }
 
 }
